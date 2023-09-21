366e0acada0665c8051c487010b89a78ce4064f6
quangphe <dd>
2023-09-21 09:24:24 +0700

    upload popup

--- Popup/popup_call_1_0_0/js/popup_call_1_0_0.js
+++ Popup/popup_call_1_0_0/js/popup_call_1_0_0.js
@@ -1,20 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const bvkn_dkcall = document.getElementsByClassName("bvkn_dkcall");
-    const popup_call_1_0_0_overlayClickform = document.getElementById("popup_call_1_0_0__overlayClickform");
-    const popup_call_1_0_0_overlay = document.getElementById("popup_call_1_0_0__overlay");
-    const popup_call_1_0_0_closePopup = document.getElementById("popup_call_1_0_0__closePopup");
-    for (const call of bvkn_dkcall) {
-        call.addEventListener("click", function () {
-            popup_call_1_0_0_overlayClickform.style.display = "block";
-            popup_call_1_0_0_overlay.style.display = "block";
-        });
-    }
-    popup_call_1_0_0_closePopup.addEventListener("click", function () {
-        popup_call_1_0_0_overlayClickform.style.display = "none";
-        popup_call_1_0_0_overlay.style.display = "none";
-    });
-    popup_call_1_0_0_overlay.addEventListener("click", function () {
-        popup_call_1_0_0_overlayClickform.style.display = "none";
-        popup_call_1_0_0_overlay.style.display = "none";
-    });
-});
+const popup_call_1_0_0 = `
+<section class="popup_call_1_0_0" id="popup_call_1_0_0">
+    <div class="popup_call_1_0_0__boxMain">
+            <div class="popup_call_1_0_0__overlay" id="popup_call_1_0_0__overlay"></div>
+            <div id="popup_call_1_0_0__overlayClickform" class="overlay_form">
+                <div class="popup_call_1_0_0__box">
+                    <article>
+                        <div class="popup_call_1_0_0__header">
+                            <div class="popup_call_1_0_0__logo"></div>
+                            <div class="popup_call_1_0_0__headerpc">
+                                <div class="popup_call_1_0_0__header-note">Bạn muốn biết chi phí của dịch vụ nào,
+                                    để lại thông tin để Kangnam tư vấn giúp bạn! </div>
+                            </div>
+                        </div>
+                        <div class="popup_call_1_0_0__content">
+                            <div class="popup_call_1_0_0__form">
+                                <div style="display:none">
+                                    <input type="text" placeholder="Họ và tên (*)" value="Yêu Cầu Gọi Lại" id="iname"
+                                        name="iname">
+                                    <textarea type="text" placeholder="Nhu cầu tư vấn" id="itext"
+                                        name="itext"></textarea>
+                                    <input id="iemail" name="iemail" value="no@email.benhvienthammykangnam.vn"
+                                        type="hidden" placeholder="Email">
+                                    <input type="hidden" id="gclid_field" name="gclid_field" value="">
+                                    <input type="hidden" id="name_campaign" name="name_campaign"
+                                        value="[Kangnam] Yêu Cầu Gọi Lại">
+                                    <input type="hidden" id="code_campaign" name="code_campaign" value="533588630">
+                                </div>
+                                <input id="imob" name="imob" type="textbox" placeholder="Nhập số điện thoại của bạn*:">
+                            </div>
+                            <div class="popup_call_1_0_0__submit">
+                                <input onclick="ants_sent_phone(this);" id="pop_sent" type="button"
+                                    value="Yêu cầu tư vấn">
+                            </div>
+                        </div>
+                        <div class="popup_call_1_0_0__headerpc">
+                            <div class="popup_call_1_0_0__header-note">* Hoặc gọi ngay hotline: 19006466</div>
+                        </div>
+                        <a id="popup_call_1_0_0__closePopup" class="popup_call_1_0_0__closePopup"></a>
+                    </article>
+                </div>
+            </div>
+        </div>
+</section>
+`;
+
+const callBtn = document.querySelectorAll(".btnkn1call, .btncallnow, .btncallme, .bvkn_dkcall");
+for (let i = 0; i < callBtn.length; i++) {
+    callBtn[i].addEventListener('click', () => {
+        document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", popup_call_1_0_0)
+        document.getElementById('popup_call_1_0_0__closePopup').addEventListener('click', () => {
+            document.getElementById('popup_call_1_0_0').remove()
+        })
+        document.getElementById('popup_call_1_0_0__overlay').addEventListener('click', () => {
+            document.getElementById('popup_call_1_0_0').remove()
+        })
+        // window.onclick = function (e) {
+        //     if (e.target == document.getElementById('popup_call_1_0_0')) {
+        //         document.getElementById('popup_call_1_0_0').remove()
+        //     }
+        // }
+    })
+}
---
+++ Popup/popup_regist_1_1_0/js/popup_regist_1_1_0.js
@@ -0,0 +1,54 @@
+const popup_regist_1_1_0 = `
+<section class="popup_regist_1_1_0" id="popup_regist_1_1_0">
+    <div class="popup_regist_1_1_0__overlay" id="popup_regist_1_1_0_overlay"></div>
+    <div id="popup_regist_1_1_0_overlayClickForm" class="popup_regist_1_1_0__overlayForm">
+        <div class="popup_regist_1_1_0__boxTv">
+            <article>
+                <div class="popup_regist_1_1_0__headerBox">
+                    <img width="200" height="100"
+                        src="https://benhvienthammykangnam.vn/css/lib/images/logo-kn-white.svg" alt="Logo Kangnam">
+                    <p class="popup_regist_1_1_0__headerTitle">Đăng ký Khuyến Mãi</p>
+                </div>
+                <div class="popup_regist_1_1_0__contentBox">
+                    <div>
+                        <input id="iname" name="iname" type="textbox" required="" placeholder="Họ tên*:">
+                        <input id="imob" name="imob" type="textbox" required="" placeholder="Điện thoại*:">
+                        <input type="hidden" style="display:none" id="iemail" name="iemail" placeholder="Email:">
+                        <textarea id="itext" type="hidden" name="itext" value=""
+                            placeholder="Dịch vụ quan tâm"></textarea>
+                        <input type="hidden" id="gclid_field" name="gclid_field" value="">
+                        <input type="hidden" id="code_campaign" name="code_campaign" value="583971142">
+                        <input type="hidden" id="name_campaign" name="name_campaign" value="[Kangnam] Sale Kangnam">
+                    </div>
+                    <div>
+                        <input class="popup_regist_1_1_0__clickBtn" id="popup_regist_1_1_0_clickSent"
+                            onclick="ants_send_contact(this);" type="button" value="HOÀN THÀNH">
+                        <div class="popup_regist_1_1_0__contact">
+                            <p>Tư vấn trực tiếp 24/7: <a href="tel:1900.6466">1900.6466</a></p>
+                        </div>
+                    </div>
+                </div>
+                <a id="popup_regist_1_1_0_closePopup"></a>
+            </article>
+        </div>
+    </div>
+</section>
+`;
+
+const callBtn = document.querySelectorAll(".btnkn1call, .btncallnow, .btncallme, .bvkn_dkcall, .notiprice");
+for (let i = 0; i < callBtn.length; i++) {
+    callBtn[i].addEventListener('click', () => {
+        document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", popup_regist_1_1_0)
+        document.getElementById('popup_regist_1_1_0_closePopup').addEventListener('click', () => {
+            document.getElementById('popup_regist_1_1_0').remove()
+        })
+        document.getElementById('popup_regist_1_1_0_overlay').addEventListener('click', () => {
+            document.getElementById('popup_regist_1_1_0').remove()
+        })
+        // window.onclick = function (e) {
+        //     if (e.target == document.getElementById('popup_call_1_0_0')) {
+        //         document.getElementById('popup_call_1_0_0').remove()
+        //     }
+        // }
+    })
+}
